3eef7d646a7e07c51fbee4821777eeca06e1dd42
lichenni <chenningli2019@gmail.com>
2023-12-15 01:05:34 -0500

    upd

---
+++ analysis/fct_to_file.py
@@ -0,0 +1,171 @@
+import subprocess
+import argparse
+import numpy as np
+import os
+
+
+def fix_seed(seed):
+    np.random.seed(seed)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "-p",
+        dest="prefix",
+        action="store",
+        default="topo4-4_traffic",
+        help="Specify the prefix of the fct file. Usually like fct_<topology>_<trace>",
+    )
+    parser.add_argument("-s", dest="step", action="store", default="5")
+    parser.add_argument(
+        "--shard", dest="shard", type=int, default=0, help="random seed"
+    )
+    parser.add_argument(
+        "--dctcp_k", dest="dctcp_k", type=int, default=30, help="DCTCP_K"
+    )
+    parser.add_argument(
+        "-t",
+        dest="type",
+        action="store",
+        type=int,
+        default=0,
+        help="0: normal, 1: incast, 2: all",
+    )
+    # parser.add_argument('-T', dest='time_limit', action='store', type=int, default=20000000000, help="only consider flows that finish before T")
+    parser.add_argument("--cc", dest="cc", action="store", default="dctcp", help="")
+    parser.add_argument(
+        "-b",
+        dest="bw",
+        action="store",
+        type=int,
+        default=25,
+        help="bandwidth of edge link (Gbps)",
+    )
+    parser.add_argument(
+        "--output_dir",
+        dest="output_dir",
+        action="store",
+        default="data/input",
+        help="the name of the flow file",
+    )
+    parser.add_argument(
+        "--scenario_dir",
+        dest="scenario_dir",
+        action="store",
+        default="AliStorage2019_exp_util0.5_lr10Gbps_nflows10000_nhosts4",
+        help="the name of the flow file",
+    )
+    args = parser.parse_args()
+
+    fix_seed(args.shard)
+    type = args.type
+    # time_limit = args.time_limit
+
+    # Please list all the cc (together with parameters) that you want to compare.
+    # For example, here we list two CC: 1. HPCC-PINT with utgt=95,AI=50Mbps,pint_log_base=1.05,pint_prob=1; 2. HPCC with utgt=95,ai=50Mbps.
+    # For the exact naming, please check ../simulation/mix/fct_*.txt output by the simulation.
+    # CCs = [
+    # 	'hpccPint95ai50log1.05p1.000',
+    # 	'hp95ai50',
+    # ]
+    # CCs = [
+    # 	args.cc,
+    # ]
+    # time_limit = int(float(args.file.split("_")[-1])*1e9)
+    time_limit = int(3000 * 1e9)
+    # step = int(args.step)
+    # res = [[i/100.] for i in range(0, 100, step)]
+    dctcp_k = args.dctcp_k
+    config_specs = "_k%d" % (dctcp_k)
+    output_dir = "%s/%s" % (args.output_dir, args.scenario_dir)
+    # for cc in CCs:
+    # file = "%s_%s.txt"%(args.prefix, cc)
+    file = "%s/fct_%s_%s%s.txt" % (output_dir, args.prefix, args.cc, config_specs)
+    # print file
+    if type == 0:
+        # cmd = "cat %s"%(file)+" | awk '{if ($4==100 && $6+$7<"+"%d"%time_limit+") {slow=$7/$8;print slow<1?1:slow, $5}}'"
+        cmd = (
+            "cat %s" % (file)
+            + " | awk '{if ($3+$4<"
+            + "%d" % time_limit
+            + ") {slow=$4/$5;print slow<1?$5:$4, $5, $2, $3, $1}}' | sort -n -k 4"
+        )
+        # print cmd
+        output = subprocess.check_output(cmd, shell=True)
+    # elif type == 1:
+    #     cmd = (
+    #         "cat %s" % (file)
+    #         + " | awk '{if ($4==200 && $6+$7<"
+    #         + "%d" % time_limit
+    #         + ") {slow=$7/$8;print slow<1?1:slow, $5}}'"
+    #     )
+    #     # print cmd
+    #     output = subprocess.check_output(cmd, shell=True)
+    # else:
+    #     cmd = (
+    #         "cat %s" % (file)
+    #         + " | awk '{$6+$7<"
+    #         + "%d" % time_limit
+    #         + ") {slow=$7/$8;print slow<1?1:slow, $5}}'"
+    #     )
+    #     # print cmd
+    #     output = subprocess.check_output(cmd, shell=True)
+
+    # up to here, `output` should be a string of multiple lines, each line is: fct, size
+    # a = output.split('\n')[:-2]
+    a = output.split("\n")[:-1]
+    n = len(a)
+    res_np = np.array(map(lambda x: x.split(), a))
+
+    print(res_np.shape)
+    # for i in range(n):
+    # 	print "%s %s %s %s %s %s"%(res_np[i,0], res_np[i,1], res_np[i,2], res_np[i,3], res_np[i,4], res_np[i,5])
+    fcts = res_np[:, 0].astype("int64")
+    i_fcts = res_np[:, 1].astype("int64")
+    flow_sizes = res_np[:, 2].astype("int64")
+    flow_arrival_times = res_np[:, 3].astype("int64")
+    flow_id = res_np[:, 4].astype("int64")
+    np.save(
+        "%s/fcts_%s_%s%s.npy" % (output_dir, args.prefix, args.cc, config_specs), fcts
+    )  # Byte
+    np.save(
+        "%s/i_fcts_%s_%s%s.npy" % (output_dir, args.prefix, args.cc, config_specs),
+        i_fcts,
+    )  # ns
+    # if not os.path.exists("%s/flow_sizes.npy" % (output_dir)):
+    #     np.save("%s/flow_sizes.npy" % (output_dir), flow_sizes)  # Byte
+    #     np.save("%s/flow_arrival_times.npy" % (output_dir), flow_arrival_times)  # ns
+    #     src_arr = np.array(map(lambda x: x[-3].split(), res_np[:, 4])).astype("int32")
+    #     dst_arr = np.array(map(lambda x: x[-3].split(), res_np[:, 5])).astype("int32")
+    #     res_arr = np.concatenate((src_arr, dst_arr), axis=1)
+    #     np.save("%s/flow_src_dst.npy" % (output_dir), res_arr)  # Byte
+
+    #     ofile = open("%s/trafficfile" % (output_dir), "w")
+    #     for i in range(n):
+    #         ofile.write("%d %d\n" % (src_arr[i], dst_arr[i]))
+    #     ofile.write("-1 -1")
+    #     ofile.close()
+
+        # os.system("rm %s/traffic.txt" % (output_dir))
+    os.system("rm %s" % (file))
+    os.system(
+        "rm %s"
+        % ("%s/mix_%s_%s%s.tr" % (output_dir, args.prefix, args.cc, config_specs))
+    )
+    os.system(
+        "rm %s"
+        % ("%s/pfc_%s_%s%s.txt" % (output_dir, args.prefix, args.cc, config_specs))
+    )
+    os.system(
+        "rm %s"
+        % ("%s/qlen_%s_%s%s.txt" % (output_dir, args.prefix, args.cc, config_specs))
+    )
+
+    ofile = open("%s/trafficfile_flow" % (output_dir), "w")
+    for i in range(n):
+        ofile.write(
+            "%d %d %d %d %d\n" % (flow_id, fcts[i], i_fcts[i], flow_sizes[i], flow_arrival_times[i])
+        )
+    ofile.write("-1 -1")
+    ofile.close()
