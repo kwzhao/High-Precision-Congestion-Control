54bf6f1a343aa878a7b177ac6e9f383d1c4b9cc1
lichenni <chenningli2019@gmail.com>
2024-02-19 14:16:26 -0500

    finish the sample space

--- simulation/consts.py
+++ simulation/consts.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 # extra params, bdp, init_window,buffer_size, enable_pfc
-# bfsz=[10,30,10]
-bfsz=[20,20,10]
+bfsz=[10,30,10]
+# bfsz=[20,20,10]
 # PFC threshold: [15, 45, 65]
 # fwin=[10, 60,1000]
 fwin=[30, 30, 1000]
-enable_pfc=[0,1]
+enable_pfc=[1,1]
 # cc
 CC_IDX_BASE=4
 # CC_LIST=["dctcp", "timely_vwin", "dcqcn_paper_vwin", "hp"]
