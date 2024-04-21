7f76d0127c9288b5e7dd3388697cb85a5f9a6578
lichenni <chenningli2019@gmail.com>
2024-04-20 23:20:49 -0400

    add reno/cubic

--- gen_path/src/main.rs
+++ gen_path/src/main.rs
@@ -43,7 +43,7 @@ fn main() -> anyhow::Result<()> {
         shard: vec![0],
         n_flows: vec![20000],
         n_hosts: vec![3],
-        shard_cc: (0..1).collect(),
+        shard_cc: (0..10).collect(),
     };
 
     // no need to change
