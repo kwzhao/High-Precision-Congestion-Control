ec8d9cb06889d99c9f6394b53d18c9a059ae6ec6
lichenni <chenningli2019@gmail.com>
2024-07-12 23:23:31 -0400

    debug traffic gen

--- traffic_gen/consts.py
+++ traffic_gen/consts.py
@@ -13,18 +13,18 @@ UNIT_G=1000000000
 min_size_in_bit=BYTE_TO_BIT * 50  # 50B
 avg_size_base_in_bit = MTU*BYTE_TO_BIT # 10KB
 
-# size_distribution_list=["exp","gaussian","lognorm","pareto"]
-size_distribution_list=["exp"]
+size_distribution_list=["exp","gaussian","lognorm","pareto"]
+# size_distribution_list=["exp"]
 size_sigma_range=[5000,50000]
 # size_sigma_range=[30000,30000]
-# ia_distribution="lognorm"
-ia_distribution="exp"
-# ias_sigma_range=[1.0,2.0]
-ias_sigma_range=[2.0,2.0]
-# load_range=[0.20,0.95]
-load_range=[0.1,0.1]
-# load_bottleneck_range=[0.20,0.95]
-load_bottleneck_range=[0.1,0.1]
+ia_distribution="lognorm"
+# ia_distribution="exp"
+ias_sigma_range=[1.0,2.0]
+# ias_sigma_range=[2.0,2.0]
+load_range=[0.20,0.95]
+# load_range=[0.1,0.1]
+load_bottleneck_range=[0.20,0.95]
+# load_bottleneck_range=[0.1,0.1]
 color_list = [
     "cornflowerblue",
     "orange",
