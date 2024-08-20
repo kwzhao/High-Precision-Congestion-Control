9ba3f2ad2ea318043f8e1e25dacafdfce880510d
lichenni <chenningli2019@gmail.com>
2024-08-20 11:29:17 -0400

    upd

--- traffic_gen/traffic_gen_synthetic.py
+++ traffic_gen/traffic_gen_synthetic.py
@@ -267,10 +267,10 @@ if __name__ == "__main__":
                 ).astype("int64")
             elif size_dist_candidate == "lognorm":
                 avg_size_in_bit = (
-                    avg_size_base_in_bit * (float(size_sigma_candidate) / 5000.0) ** 2.4
+                    avg_size_base_in_bit * (float(size_sigma_candidate) / 5000.0) ** 2.5
                 )
-                # size_sigma = 0.8 + (60000 - float(size_sigma_candidate)) / 30000
-                size_sigma = 2.0
+                size_sigma = 0.8 + (60000 - float(size_sigma_candidate)) / 30000
+                # size_sigma = 2.0
                 # flow size
                 mu = np.log(avg_size_in_bit - min_size_in_bit) - (size_sigma**2) / 2
                 f_sizes_in_byte = (
@@ -284,9 +284,9 @@ if __name__ == "__main__":
                 ).astype("int64")
             elif size_dist_candidate == "pareto":
                 avg_size_in_bit = (
-                    avg_size_base_in_bit * (float(size_sigma_candidate) / 5000.0) ** 2.5
+                    avg_size_base_in_bit * (float(size_sigma_candidate) / 5000.0) ** 3
                 )
-                size_sigma = avg_size_in_bit // 2.5
+                size_sigma = avg_size_in_bit // 2
                 func = lambda x: 5 - np.power(
                     1 + x * (avg_size_in_bit - min_size_in_bit) / size_sigma, 1 / x
                 )
