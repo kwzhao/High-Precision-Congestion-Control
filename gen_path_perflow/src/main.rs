dd23cf50a9fd287499801568ffbc5fe3ddc67062
lichenni <chenningli2019@gmail.com>
2024-09-25 11:51:35 -0400

    upd remain size tracking

--- gen_path_perflow/src/main.rs
+++ gen_path_perflow/src/main.rs
@@ -15,7 +15,7 @@ struct Parameters {
 pub struct Main {
     #[clap(long, default_value = "/data1/lichenni/software/anaconda3/envs/py39/bin/python")]
     python_path: PathBuf,
-    #[clap(long, default_value = "/data2/lichenni/perflow_link_size")]
+    #[clap(long, default_value = "/data2/lichenni/perflow_link_size_test")]
     output_dir: PathBuf,
 }
 
@@ -45,12 +45,12 @@ fn main() -> anyhow::Result<()> {
 
     // config for debugging
     let params = Parameters {
-        // shard: vec![0],
-        shard: (0..2000).collect(),
+        shard: vec![0],
+        // shard: (0..1000).collect(),
         n_flows: vec![2000],
+        // n_flows: vec![10000],
         n_hosts: vec![21],
         // n_hosts: vec![5],
-        // n_hosts: vec![3],
         shard_cc: vec![0],
         max_inflight_flows: vec![0],
     };
