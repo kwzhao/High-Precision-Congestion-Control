d68f3ada1958c67b0f112f7607db6570980bb594
lichenni <chenningli2019@gmail.com>
2024-11-15 13:08:21 -0500

    upd

--- traffic_gen/traffic_gen_synthetic_dist.py
+++ traffic_gen/traffic_gen_synthetic_dist.py
@@ -135,10 +135,10 @@ if __name__ == "__main__":
             )
         elif size_dist_candidate == "lognorm":
             avg_size_in_bit = (
-                avg_size_base_in_bit * (float(size_sigma_candidate) / 5000.0) ** 3
+                avg_size_base_in_bit * (float(size_sigma_candidate) / 5000.0) ** 2.8
             )
             # size_sigma = 0.8 + (60000 - float(size_sigma_candidate)) / 30000
-            size_sigma = 2.2
+            size_sigma = 2
             # flow size
             mu = np.log(avg_size_in_bit - min_size_in_bit) - (size_sigma**2) / 2
             f_sizes_in_byte = (
