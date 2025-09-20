cb98d73f36500cf844dcadfa5ed1f96c29205d35
liecn <chenningli2019@gmail.com>
2025-09-20 19:31:34 -0400

    upd

--- ns-3.39/configure.sh
+++ ns-3.39/configure.sh
@@ -1,1 +1,3 @@
+export CC=gcc-9
+export CXX=g++-9
 CXXFLAGS=-w ./ns3 configure --build-profile=optimized --enable-examples --disable-tests --enable-python-bindings --disable-werror --disable-warnings
